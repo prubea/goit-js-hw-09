4e832e5fa04908917ede025b4ea255d6ad013042
Beata Pruszczyk <prubea@gmail.com>
2022-08-09 18:52:54 +0200

    01-color-switcher
    Zadanie 1
    Zmiana koloru body co 1 sekunda

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+startBtn.addEventListener("click", handleEvent);
+let timerId = null;
+function handleEvent() {
+  timerId = setInterval(() => {
+    document.body.style.background = getRandomHexColor();
+  }, 1000);
+  startBtn.disabled = true;
+}
+
+stopBtn.addEventListener("click", stopEvent);
+function stopEvent() {
+  clearInterval(timerId);
+  console.log(`Interval with id ${timerId} has stopped!`);
+  startBtn.disabled = false;
+};
+
+
+
 
