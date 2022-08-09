329e27e286d8e9177ff8b9f935372c7d45ca0ed1
prubea <32736543+prubea@users.noreply.github.com>
2022-08-09 16:53:28 +0000

    Deploying to gh-pages from @ prubea/goit-js-hw-09@4e832e5fa04908917ede025b4ea255d6ad013042 🚀

---
+++ 01-color-switcher.5140e76f.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop");t.addEventListener("click",(function(){n=setInterval((function(){document.body.style.background="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.disabled=!0}));var n=null;e.addEventListener("click",(function(){clearInterval(n),console.log("Interval with id ".concat(n," has stopped!")),t.disabled=!1}))}();
+//# sourceMappingURL=01-color-switcher.5140e76f.js.map
---
+++ 01-color-switcher.cd442162.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop");t.addEventListener("click",(function(){n=setInterval((()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3),t.disabled=!0}));let n=null;e.addEventListener("click",(function(){clearInterval(n),console.log(`Interval with id ${n} has stopped!`),t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.cd442162.js.map
