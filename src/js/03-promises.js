3fc779af4ab422415456646356dec76ed7c144e8
Beata Pruszczyk <prubea@gmail.com>
2022-08-11 20:01:49 +0200

    Update 03-promises.js
    Zadanie 3
    skrypt, który po wysłaniu formularza wywoła funkcję createPromise(position, delay) tyle razy, ile wprowadzono w pole amount

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,54 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  delay = delay.value;
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      const shouldResolve = Math.random() > 0.3;
+      if (shouldResolve) {
+      // Fulfill
+        resolve({ position, delay });
+      }
+      else {
+      // Rejectreject({ position, delay });
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+const form = document.querySelector('.form');
+const { delay, step, amount } = document.querySelector('form');
+let position = null;
+
+// submit form handleEvent
+form.addEventListener('submit', handleEvent);
+
+function handleEvent(e) {
+  e.preventDefault();
+
+  // loop for promise generator
+  for (let i = 0; i < amount.value; i++) {
+    setTimeout(() => {
+      position = i + 1;
+
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`,
+          {
+            useIcon: false,
+          }
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`,
+          {
+            useIcon: false,
+          }
+        );
+      });
+    }, i * step.value);
   }
 }
